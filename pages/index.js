b38e5465592401b1d8f12b2ffbb73adeb1c0fbb7
CHR <chasehrensberger@gmail.com>
2022-08-07 21:19:46 -0400

    format

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { holidayList } from '../data/holidays';
-import moment from 'moment';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { holidayList } from "../data/holidays";
+import moment from "moment";
 
 export default function Home() {
+	const todaysHolidayObjects = holidayList.filter(
+		(holidayObject) => holidayObject.Day == moment().format("MMM D")
+	);
+	const todaysHolidays = todaysHolidayObjects.map(
+		(todayHolidayObject) => todayHolidayObject.Holiday
+	);
 
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Luke's Holiday Tracker</title>
+				<meta name="description" content="Luke's holiday tracker" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  const todaysHolidayObjects = holidayList.filter((holidayObject) => holidayObject.Day == moment().format("MMM D"));
-  const todaysHolidays = todaysHolidayObjects.map(todayHolidayObject => todayHolidayObject.Holiday);
-  
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Luke's Holiday Tracker</title>
-        <meta name="description" content="Luke's holiday tracker" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <div>{moment().format("MMM D")}</div>
-        <div>{JSON.stringify(todaysHolidays)}</div>
-      </main>
-       
-    </div>
-  )
+			<main className={styles.main}>
+				<div>{moment().format("MMM D")}</div>
+				<div>{JSON.stringify(todaysHolidays)}</div>
+			</main>
+		</div>
+	);
 }
